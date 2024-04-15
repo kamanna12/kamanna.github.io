4f1544a325bc00a4199d4ec6b4a2d8f4a545caf7
kamanna12 <kamanna@uncc.edu>
2024-04-14 21:17:39 -0400

    e

---
+++ itis3135/ClientProject/scripts/base.js
@@ -0,0 +1,4 @@
+function toggleNav() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display === 'none' ? navLinks.style.display = 'flex' : navLinks.style.display = 'none';
+  }  
